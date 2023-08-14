e8be4bb73a89094d71c4f4d9ced0eabe4886c488
Syed Razauddin Shahlal <96375316+syedshahlal@users.noreply.github.com>
2023-08-14 05:06:47 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from typing import List
+
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path: str)-> List[str]:
+    '''
+    Reads requirements.txt file and returns a list of requirements
+    '''
+    requirements = []
+    with open(file_path) as f:
+        requirements = f.readlines()
+        [req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    description='My first ML project',
+    author='Syed',
+    author_email= 'sr.shahlal@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt'),
+
+
+
+)
