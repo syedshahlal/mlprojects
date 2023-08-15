9bd1c0d1f5fac41830f8297c69fd0581cd72eac6
Syed Razauddin Shahlal <96375316+syedshahlal@users.noreply.github.com>
2023-08-15 03:12:46 -0400

    data transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,8 @@ import numpy as np
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -18,7 +20,7 @@ class DataIngestion:
     def __init__(self):
         self.ingestion_config = DataIngestionConfig()
 
-    def initiatte_data_ingestion(self):
+    def initiate_data_ingestion(self):
         try:
             logging.info("Data ingestion started")
             df=pd.read_csv('notebook\data\stud.csv')
@@ -39,12 +41,14 @@ class DataIngestion:
             return(
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path,
-                self.ingestion_config.raw_data_path
             ) 
         except Exception as e:
             logging.error("Data ingestion failed")
             raise CustomException(e,sys)
         
-if __name__ == "__main__":
-    data_ingestion = DataIngestion()
-    data_ingestion.initiatte_data_ingestion()
+if __name__=="__main__":
+    obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
